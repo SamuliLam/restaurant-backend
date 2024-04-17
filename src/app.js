8124e18502da4ab74872101c3c7cfe18601d713d
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-17 17:39:57 +0300

    Making base structure, app.js, index.js.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 import express from 'express';
 import apiRouter from './api/index.js';
 
-
 const app = express();
 
 app.use('/api/v1', apiRouter);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
-});
-
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
 
 export default app;
