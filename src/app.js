266b9ead05ddb781581856dec6660e6ca467548a
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-17 17:13:21 +0300

    Backend tiedostojen lisäys ja alku

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const hostname = '127.0.0.1';
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
