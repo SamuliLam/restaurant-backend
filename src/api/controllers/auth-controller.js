1aba51c5f201e699706b50bd168948029d688ace
stefu <stefanos.thomas@metropolia.fi>
2024-04-23 16:49:56 +0300

    changes

--- src/api/controllers/auth-controller.js
+++ src/api/controllers/auth-controller.js
@@ -6,17 +6,17 @@ import "dotenv/config";
 const postLogin = async (req, res) => {
   const user = await getUserByEmail(req.body.email);
   if (!user) {
-    return res.status(401);
+    return res.status(401).json({ message: 'Login failed: User not found' });
   }
 
   if (!bcrypt.compareSync(req.body.password, user.password)) {
-    return res.status(401);
+    return res.status(401).json({ message: 'Login failed: Incorrect password' });
   }
   delete user.password;
 
   const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'});
 
-  res.json({user, token});
+  res.json({user, token, message: 'Login successful'});
 };
 
 const getMe = async (req, res) => {
