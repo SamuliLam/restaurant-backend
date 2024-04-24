20083a683cb1be832da436accf91ee180b9031ac
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-24 15:23:40 +0300

    test

--- src/app.js
+++ src/app.js
@@ -3,10 +3,7 @@ import cors from 'cors';
 import api from './api/index.js';
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:63342'
-}));
-// test
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
