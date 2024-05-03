5d02e21bca0deb54539a0637abae666fe2b2836b
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-05-03 20:38:02 +0300

    asd

--- src/api/routes/product-router.js
+++ src/api/routes/product-router.js
@@ -18,7 +18,7 @@ productRouter.route('/')
 // TODO: Add middlewares
 productRouter.route('/:id')
   .get(getProductById)
-  .put(authenticateToken, putProduct)
+  .post(authenticateToken, putProduct)
   .delete(authenticateToken, deleteProduct);
 
 export default productRouter;
