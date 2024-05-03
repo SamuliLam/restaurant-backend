674b0ae6f5f4c8986c023a74d127b3d73abfd693
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-05-03 20:36:51 +0300

    asd

--- src/app.js
+++ src/app.js
@@ -3,12 +3,7 @@ import cors from 'cors';
 import api from './api/index.js';
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
